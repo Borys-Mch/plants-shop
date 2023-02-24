2bc3dbd7dbeec9f3dd7c0423a3fc12d0c792b362
Borys Mch <borys.mch@gmail.com>
2023-02-24 13:40:49 +0200

    Separeted theme

--- src/pages/_app.js
+++ src/pages/_app.js
@@ -1,47 +1,7 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import { extendTheme } from "@chakra-ui/react";
 import "@/styles/globals.css";
 import Layout from "@/components/Layout";
-
-const theme = extendTheme({
-  styles: {
-    global: {
-      body: {
-        fontFamily: "Work Sans",
-        lineHeight: "base",
-      },
-    },
-  },
-  sizes: {
-    container: {
-      "2xl": "1488px",
-    },
-  },
-  components: {
-    Heading: {
-      variants: {
-        footer: {
-          color: "#FFF",
-          fontSize: "24px",
-          fontWeight: "700",
-          lineHeight: "2.1",
-          paddingTop: "20px",
-        },
-      },
-    },
-    Text: {
-      variants: {
-        footer: {
-          color: "#FFF",
-          fontSize: "16px",
-          fontWeight: "500",
-          lineHeight: "1.5",
-          paddingTop: "16px",
-        },
-      },
-    },
-  },
-});
+import { theme } from "@/styles/theme";
 
 export default function App({ Component, pageProps }) {
   return (
